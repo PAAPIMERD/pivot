8551ac6877e423777f5901f650e1c771d370b57d
PAAPIMERD <97976194+PAAPIMERD@users.noreply.github.com>
2024-05-15 01:02:57 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -2,32 +2,47 @@
 import time
 from kite_trade import *
 from datetime import datetime
-import neo_api_client
-from neo_api_client import NeoAPI
+
 enctoken = "2g5rlyFLveEkZwoo3/m7I9jI3XsUSfcV3uIv22/9uZcDtNfc6laFAaGFo8yKOJFayPpassobrp9oKAotEHM2K49D70hUbikGP8GzFic/voD0N6tWHriOIA=="
 kite = KiteApp(enctoken=enctoken)
-cons_key = "vvdHaGEFcURbb36F0DVJfDkW0CQa"
-cons_sec = "DgTu4TNBTW6yLXb7nF08knjY_HUa"
-mobile = "+916303008951"
-paswd = "Avks@1234"
-mpin = "271707"
 
-client = NeoAPI(consumer_key=cons_key, consumer_secret=cons_sec, environment='prod',
-                access_token=None, neo_fin_key=None)
 
-client.login(mobilenumber=mobile, password=paswd)
-client.session_2fa(OTP=mpin)
 
 def buy():
-  client.place_order(exchange_segment="nse_cm", product="MIS", price="", order_type="MKT", quantity="1", validity="DAY", trading_symbol="ONGC-EQ",
-                       transaction_type="B", amo="NO", disclosed_quantity="0", market_protection="0", pf="N",
-                       trigger_price="0", tag=None)
-  
+  order = kite.place_order(variety=kite.VARIETY_REGULAR,
+                         exchange=kite.EXCHANGE_NSE,
+                         tradingsymbol="ONGC",
+                         transaction_type=kite.TRANSACTION_TYPE_BUY,
+                         quantity=1,
+                         product=kite.PRODUCT_MIS,
+                         order_type=kite.ORDER_TYPE_MARKET,
+                         price=None,
+                         validity=None,
+                         disclosed_quantity=None,
+                         trigger_price=None,
+                         squareoff=None,
+                         stoploss=None,
+                         trailing_stoploss=None,
+                         tag="TradeViaPython")
+
 def sell():
-  client.place_order(exchange_segment="nse_cm", product="MIS", price="", order_type="MKT", quantity="1", validity="DAY", trading_symbol="ONGC-EQ",
-                       transaction_type="S", amo="NO", disclosed_quantity="0", market_protection="0", pf="N",
-                       trigger_price="0", tag=None)
+  order = kite.place_order(variety=kite.VARIETY_REGULAR,
+                         exchange=kite.EXCHANGE_NSE,
+                         tradingsymbol="ONGC",
+                         transaction_type=kite.TRANSACTION_TYPE_SELL,
+                         quantity=1,
+                         product=kite.PRODUCT_MIS,
+                         order_type=kite.ORDER_TYPE_MARKET,
+                         price=None,
+                         validity=None,
+                         disclosed_quantity=None,
+                         trigger_price=None,
+                         squareoff=None,
+                         stoploss=None,
+                         trailing_stoploss=None,
+                         tag="TradeViaPython")
   
+
 
 
 
